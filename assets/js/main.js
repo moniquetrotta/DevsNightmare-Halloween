53243a1207eec36b685373600d999ad60c2bff01
moniquetrotta <monique.trotta@gmail.com>
2023-10-13 16:26:23 -0300

    teste base primeiro commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const BASE_URL = 'http://localhost:3000/assets/img';
+const imgBtn = document.getElementById('change-img');
+
+const getImg = async () => {
+  const data = await fetch(BASE_URL)
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+  return data.img;
+};
+
+const loadImg = async () => {
+  const terrorImg = document.getElementById('img');
+  terrorImg.src = await getImg();
+};
+
+imgBtn.addEventListener('click', loadImg);
+
+loadImg();
---
+++ assets/server/server.js
@@ -0,0 +1,24 @@
+// Importe o Express
+const express = require('express');
+
+// Crie um servidor Express
+const app = express();
+
+// Crie uma rota para obter uma imagem aleatória
+app.get('/api/imagem', (req, res) => {
+  // Crie uma lista de imagens
+  const imagens = fs.readdirSync(__dirname + '/imagens');
+
+  // Selecione uma imagem aleatória
+  const imagem = imagens[Math.floor(Math.random() * imagens.length)];
+
+  // Retorne a imagem
+  res.json({
+    imagem: `/imagens/${imagem}`,
+  });
+});
+
+// Inicie o servidor
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
